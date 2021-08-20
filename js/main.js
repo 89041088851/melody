51f4bbf4b4c3a29faf5632231d4e7183279568c1
Bogdan <bogdan.kosnyrev@outlook.com>
2021-08-20 22:53:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,10 +3,26 @@ $(document).ready(function () {
   var floorPath = $('.main__svg-image path');
   var counterUp = $('.main-info__btn-up');
   var counterDown = $('.main-info__btn-down');
+  var modal = $('.modal');
+  var modalCloseButton = $('.modal__btn-close');
+  var viewFlatsButton = $('.main-info__view-flats');
+
   floorPath.on('mouseover', function (){
     floorPath.removeClass('main__svg-image__current-floor');
     currentFloor = $(this).attr('data-floor');
     $('.main-info__counter').text(currentFloor);
+  });
+
+  floorPath.on('click', function () {
+    modal.addClass('is-open');
+  });
+
+  modalCloseButton.on('click', function (){
+    modal.removeClass('is-open');
+  });
+
+  viewFlatsButton.on('click', function (){
+    modal.addClass('is-open');
   });
 
   counterUp.on('click', function () {
